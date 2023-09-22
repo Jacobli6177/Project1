4c980a2e46a00475b0bf3995c7cdbd8e8b85badc
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2023-09-22 00:38:29 -0400

    new

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -11,67 +11,67 @@ const Card = () => {
                 <tr>
                     <td classname = 'bigbox'> 
                         <img class= 'pic1' src = '../src/images/danta.jpg'></img>
-                        <Event food ='Danta'/>
-                        <button > View Background</button>
+                        <Event food ='Eggtart'/>
+                        <button> <a href="https://en.wikipedia.org/wiki/Egg_tart" target="_blank">View Background</a></button>
                     </td>
                     <td>
                     <img class= 'pic1' src = '../src/images/bibimbap.jpg'></img>
                         <Event food = 'Bibimbap'/>
-                        <button> View Background</button>
+                        <button><a href ="https://en.wikipedia.org/wiki/Bibimbap" target ="_blank">View Background</a></button>
                     </td>
                     <td>
                     <img class= 'pic1' src = '../src/images/cheesecake.jpg'></img>
                         <Event food = 'Cheesecake'/>
-                        <button> View Background</button>
+                        <button><a href ="https://en.wikipedia.org/wiki/Cheesecake" target ="_blank">View Background</a></button>
                     </td>
                     <td>
                         <img class= 'pic1' src = '../src/images/pekingduck.jpg'></img>
                         <Event food = 'Pekingduck'/>
-                        <button> View Background</button>
+                        <button><a href ="https://en.wikipedia.org/wiki/Peking_duck" target ="_blank">View Background</a></button>
                     </td>
                 </tr>
                 <tr>
                     <td>
                     <img class= 'pic1' src = '../src/images/Food.jpg'></img>
                     <Event food ='Brunch'/>
-                    <button> View Background</button>
+                    <button><a href ="https://en.wikipedia.org/wiki/Food" target ="_blank">View Background</a></button>
                     </td>
                     <td>
                         <img class= 'pic1' src = '../src/images/cheeseburger.jpg'></img>
                         <Event food ='Cheeseburger'/>
-                        <button> View Background</button>
+                        <button><a href ="https://en.wikipedia.org/wiki/Cheeseburger" target ="_blank">View Background</a></button>
                     </td>
                     <td>
                         <img class= 'pic1' src = '../src/images/crossant.jpg'></img>
-                        <Event food = 'Crossant'/>
-                        <button> View Background</button>
+                        <Event food = 'Croissant'/>
+                        <button><a href ="https://en.wikipedia.org/wiki/Croissant" target ="_blank">View Background</a></button>
                     </td>
                     <td>
                         <img class= 'pic1' src = '../src/images/soupdumpling.jpg'></img>
                         <Event food ='SoupDumpling'/>
-                        <button> View Background</button>
+                        <button><a href ="https://therecipecritic.com/soup-dumplings/" target ="_blank">View Background</a></button>
                     </td>
                 </tr>
                 <tr>
                     <td>
                         <img class= 'pic1' src = '../src/images/Empanadas.jpg'></img>
                         <Event food ='Empanadas'/>
-                        <button> View Background</button>
+                        <button><a href ="https://en.wikipedia.org/wiki/Empanadas" target ="_blank">View Background</a></button>
                     </td>
                     <td>
                         <img class= 'pic1' src = '../src/images/sushi.jpg'></img>
                         <Event food ='Sushi'/>
-                        <button> View Background</button>
+                        <button><a href ="https://en.wikipedia.org/wiki/Sushi" target ="_blank">View Background</a></button>
                     </td>
                     <td>
                         <img class= 'pic1' src = '../src/images/pizza.jpg'></img>
                         <Event food = 'Pizza'/>
-                        <button> View Background</button>
+                        <button><a href ="https://en.wikipedia.org/wiki/Pizza" target ="_blank">View Background</a></button>
                     </td>
                     <td>
                         <img class= 'pic1' src = '../src/images/wagyu.jpg'></img>
                         <Event food = "Wagyu"/>
-                        <button> View Background</button>
+                        <button><a href ="https://en.wikipedia.org/wiki/Wagyu" target ="_blank">View Background</a></button>
                     </td>
                 </tr>
             </tbody>
